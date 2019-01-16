de6a6db5bc5856d285f7cb56768dc4cb254aac9d
licong <licong@360.net>
2019-01-16 19:53:55 +0800

    添加设置glide的脚本

--- glide.sh
+++ glide.sh
@@ -1,12 +1,4 @@
-parse_git_branch() {
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-}
-append() {
-  export PATH=$PATH:$1
-}
-export PS1="\u@\h \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
-export GOPROXY="https://athens.azurefd.net"
-append $GOPATH/bin
+#!/usr/bin/env bash
 glide mirror set https://golang.org/x/mobile https://github.com/golang/mobile --vcs git
 glide mirror set https://golang.org/x/crypto https://github.com/golang/crypto --vcs git
 glide mirror set https://golang.org/x/net https://github.com/golang/net --vcs git
@@ -14,4 +6,3 @@ glide mirror set https://golang.org/x/tools https://github.com/golang/tools --vc
 glide mirror set https://golang.org/x/text https://github.com/golang/text --vcs git
 glide mirror set https://golang.org/x/image https://github.com/golang/image --vcs git
 glide mirror set https://golang.org/x/sys https://github.com/golang/sys --vcs git
-source /home/hellojukay/.local/bin/bashmarks.sh
