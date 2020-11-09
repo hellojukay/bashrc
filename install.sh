0016ea60f632ef21fb1c745b60be24cd442989ee
licong <licong@qianxin.com>
2020-11-09 09:39:39 +0000

    自动安装 tmux

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo pacman -S python python-pip fontconfig tmux
 pip install ansible --user -i https://mirrors.aliyun.com/pypi/simple/
 export PATH=$PATH:$HOME/.local/bin
 ansible-playbook install.yml
